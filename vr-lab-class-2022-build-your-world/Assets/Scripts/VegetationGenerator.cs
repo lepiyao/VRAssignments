5596b1fe180b6e41ebaa28877dbe1466a3009e50
Lepiyao <levihanny_96@hotmail.com>
2022-10-27 22:19:00 +0200

    Task 1.2 1-3 Done

--- vr-lab-class-2022-build-your-world/Assets/Scripts/VegetationGenerator.cs
+++ vr-lab-class-2022-build-your-world/Assets/Scripts/VegetationGenerator.cs
@@ -29,11 +29,20 @@ public class VegetationGenerator : MonoBehaviour
     {
         // TODO: Exercise 1.2 -> 3.) 
         // check & handle "reset" to regenerate vegetation instances
+        if (reset == true) {
+            ClearVegetation();
+            GenerateVegetation();
+            reset = false;
+        }
     }
 
     void ClearVegetation()
     {
         // TODO: part of Exercise 1.2 -> 3.)  
+        for (int i=0; i < instances.Count; i++) {
+            Destroy(instances[i]);
+        }
+        instances.Clear();
     }
 
     void GenerateVegetation()
@@ -42,7 +51,21 @@ public class VegetationGenerator : MonoBehaviour
         // Instantiate & transform random "vegetationPrefab"
 
         // your code here
+        for (int i=0; i < numObjects;i++) {
+            // Set the Transformation using Vector3
+            Vector3 rangeMinMax = new Vector3(Random.Range(vegetationBoundsMin.x, vegetationBoundsMax.x),
+            Random.Range(vegetationBoundsMin.y, vegetationBoundsMax.y),
+            Random.Range(vegetationBoundsMin.z, vegetationBoundsMax.z));
+            
+            //Choose random prefabs using the number of item list in the vegetationPrefabs
+            int item = Random.Range(0, vegetationPrefabs.Count);
 
+            //Create the gameObj and set the parent to the transform and add to the instances game object list
+            GameObject gObj = Instantiate(vegetationPrefabs[item], rangeMinMax, Quaternion.Euler(0, Random.Range(0, 30), 0));
+            gObj.transform.SetParent(transform);
+            instances.Add(gObj);
+        }
+        
         // Collisions need to be resolved at a later time,
         // because Unity physics loop (Unity-internal evaluation of collisions)
         // runs separate from Update() loop
@@ -56,11 +79,31 @@ public class VegetationGenerator : MonoBehaviour
 
         // TODO: Exercise 1.2 -> 2.)
         // check & handle bounds intersection of each instance with "restrictedBounds"
-
         // your code here
 
-        // resolve again (delayed), after new random transform applied to colliding instances
-        if (resolveAgain)
+        Collider col1, col2;
+        Vector3 rangeMinMax = new Vector3(Random.Range(vegetationBoundsMin.x, vegetationBoundsMax.x),
+            Random.Range(vegetationBoundsMin.y, vegetationBoundsMax.y),
+            Random.Range(vegetationBoundsMin.z, vegetationBoundsMax.z));
+
+        for (int i = 0; i < instances.Count; i++)
+        {
+            col2 = instances[i].GetComponent<Collider>();
+
+            for (int j = 0; j < restrictedBounds.Count; j++)
+            {
+                col1 = restrictedBounds[j].GetComponent<Collider>();
+                if (col1.bounds.Intersects(col2.bounds))
+                {
+                    Debug.Log("Bounds intersecting");
+                    instances[i].transform.Translate(rangeMinMax);
+                }
+            }
+
+        }
+
+            // resolve again (delayed), after new random transform applied to colliding instances
+            if (resolveAgain)
             StartCoroutine(ResolveCollisions());
     }
 
